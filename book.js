9fab8a928e1aac019c2574e81d2d4706378176d7
duc tran <kristjantran@yahoo.com>
2018-03-02 10:24:50 +0000

    able to put book.csv to database now

--- book.js
+++ book.js
@@ -1,15 +1,63 @@
+require('dotenv').config();
+
 const csvdata = require('csvdata');
 const express = require('express');
+const { Client } = require('pg');
 
 const router = express.Router();
 
 const booksPath = './data/books.csv';
 
+const connectionString = process.env.DATABASE_URL;
+
+async function query(q, values = []) {
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  let result;
+
+  try {
+    result = await client.query(q, values);
+  } catch (err) {
+    throw err;
+  } finally {
+    await client.end();
+  }
+
+  return result;
+}
+
+async function insertCategory(category) {
+  const q = 'INSERT INTO categories (name) VALUES ($1) ON CONFLICT (name) DO UPDATE SET name=$1 RETURNING *';
+  const result = await query(q, [category]);
+  return result.rows;
+}
+
+async function insertBooks(title, isbn13, author, description, category, isbn10, published, pagecount, language) {
+  const q = 'INSERT INTO books (title, isbn13, author, description, category, isbn10, published, pagecount, language) '
+          + 'VALUES($1, $2, $3, $4, (SELECT cateid FROM categories WHERE categories.name=$5), $6, $7, $8, $9) '
+          + 'RETURNING *';
+  const result = await query(q, [title, isbn13, author, description, category, isbn10, published, pagecount, language]);
+  return result.rows;
+}
+
 router.get('/', (req, res) => {
   csvdata.load(booksPath, { delimiter: ',' })
     .then((result) => {
+      Promise.all(result)
+        .then(async (data) => {
+          for (let i = 0; i !== data.length; i += 1) {
+            await insertCategory(data[i].category);
+          }
+          for (let j = 0; j !== data.length; j += 1) {
+            await insertBooks(data[j].title, data[j].isbn13, data[j].author, data[j].description,
+              data[j].category, data[j].isbn10, data[j].published, data[j].pagecount, data[j].language);
+          }
+        })
+        .then(() => console.info('insertions has been completed'));
       res.status(200).json(result);
-    });
+    })
+    .catch(err => console.warn(err));
 });
 
 module.exports = router;
--- schema.sql
+++ schema.sql
@@ -13,14 +13,14 @@ CREATE TABLE IF NOT EXISTS categories (
 
 CREATE TABLE IF NOT EXISTS books (
     bookId          serial primary key,
-    title           varchar(100) UNIQUE NOT NULL,
+    title           text UNIQUE NOT NULL,
     isbn13          varchar(13) CHECK (char_length(isbn13) = 13) UNIQUE NOT NULL,
     author          varchar(65),
     description     text,
     category        int NOT NULL,
     isbn10          varchar(10),
-    published     varchar(25),
-    pagecount       int,        
+    published       varchar(25),
+    pagecount       varchar(25),
     language        varchar(2),
     FOREIGN KEY (category) REFERENCES categories(cateId)
 );
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS readbook (
     userId          int NOT NULL,
     bookId          int NOT NULL,
     star            smallint CHECK (star >= 1 AND star <= 5) NOT NULL,
-    feel            text,
+    review            text,
     FOREIGN KEY (userId) REFERENCES users(userId),
     FOREIGN KEY (bookId) REFERENCES books(bookId)
 );
