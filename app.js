eec7b872a988f10a3929b4efa0d91619c90c345a
Hoai Nam Duc Tran <kristjantran@gmail.com>
2018-04-12 07:33:02 +0000

    added options for cors preflight request

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.status(200);
+  }
   next();
 });
 
